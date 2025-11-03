8ad25554739c628b3427ee4021ffb23a42680442
goodguykf <kuanfengtan@gmail.com>
2025-11-03 23:21:20 +0800

    Minor Cosmetic touch up
    The style is wrong.
    
    Update the code to fit.

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -17,7 +17,7 @@ public class ListCommand extends Command {
 
 
     @Override
-    public CommandResult execute(Model model) throws CommandException{
+    public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
         model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
         if (model.getFilteredPersonList().isEmpty()) {
